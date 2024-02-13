75bc4fe92032eec8790870c7ca1b9c185f3e64f5
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-02-13 12:50:11 +0530

    this method for getuser & user info help of fetch api

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-const result =document.getElementById('result')
-const filter =document.getElementById('filter')
-const listItems=[]
+// enter your javascript code here
+const result = document.getElementById('result');
+const filter = document.getElementById('filter');
+const listItems = [];
 
-    getdata()
-async function getdata(){
-    const res=await fetch('https://randomuser.me/api?results=50')
+getdata();
 
-      const data = await res.json()
+filter.addEventListener('input', (e) => filterData(e.target.value));
 
-      console.log(data)
+async function getdata() {
+    try {
+        const res = await fetch('https://randomuser.me/api?results=50');
+        const { results } = await res.json();
+
+        result.innerHTML = '';
+
+        results.forEach(user => {
+            const li = document.createElement('li');
+
+            listItems.push(li);
+
+            li.innerHTML = `
+                <img src="${user.picture.large}" alt="${user.name.first}">
+                <div class="user-info">
+                    <h4>${user.name.first} ${user.name.last}</h4>
+                    <p>${user.location.city}, ${user.location.country}</p>
+                </div>
+            `;
+
+            result.appendChild(li);
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
 
-
+function filterData(searchTerm) {
+    listItems.forEach(item => {
+        if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+            item.classList.remove('hide');
+        } else {
+            item.classList.add('hide');
+        }
+    });
+}
